13966bfa1574853283b4ad92cfe311ad4bd66091
Francis Tseng <f@frnsys.com>
2015-06-12 17:54:20 +0000

    added script to export a sample of events to json

---
+++ export.py
@@ -0,0 +1,41 @@
+import sys
+import json
+import random
+from focusgroup.models import Event
+
+
+if __name__ == '__main__':
+    n = int(sys.argv[1])
+    out = sys.argv[2]
+
+    N = Event.objects.count()
+    if n > N:
+        n = N
+
+    print('Sampling {} events'.format(n))
+    sample = []
+    while len(sample) < n:
+        i = random.randint(0, N)
+        e = Event.objects[i]
+        if e in sample:
+            continue
+        sample.append(e)
+
+    data = []
+    for e in sample:
+        data.append({
+            'title': e.title,
+            'articles': [{
+                'title': a.title,
+                'body': a.body,
+                'url': a.url,
+                'image': a.image,
+                'published': a.published.timestamp()
+            } for a in e.articles]
+        })
+
+
+    with open(out, 'w') as f:
+        json.dump(data, f)
+
+    print('Sample output to {}'.format(out))
